401f47cdc3d3bedd09616800220e19b9a4666a8e
ryuuki98 <ryuuki98@naver.com>
2024-04-01 18:13:18 +0900

    create: Unit class를 상속받는 Player class 생성

---
+++ textRPG/src/textRPG/Player.java
@@ -0,0 +1,16 @@
+package textRPG;
+
+public class Player extends Unit{
+	
+	
+
+	public Player(String name, int hp, int power) {
+		super(name, hp, power);
+	}
+
+	@Override
+	public void skill() {
+		
+	}
+
+}
